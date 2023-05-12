a95ef07fb0129bbc2809925f07ac34ca993a57ef
surajkh <90398336+SurajKH@users.noreply.github.com>
2023-05-12 19:20:34 +0530

    Add files via upload

---
+++ backend/com.demo.example/InventoTrackerSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoTrackerSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoTrackerSpringApplication.class, args);
+	}
+
+}
