c4e0ab6f94dbb18bc202ec60f6f0e7af0363909c
surajkh <90398336+SurajKH@users.noreply.github.com>
2023-05-13 09:40:09 +0530

    Add files via upload

---
+++ frontend/inventoryfrontendapplication/src/Components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <React.Fragment>
+
+<footer className="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
+    <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-center bg-black">
+      <span className="text-md text-white sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" className="hover:underline">InventoTracker™</a>. All Rights Reserved.
+    </span>
+        
+       </div>
+</footer>
+    </React.Fragment>
+  )
+}
+
+export default Footer;
