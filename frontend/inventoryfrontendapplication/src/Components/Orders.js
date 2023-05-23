bb3933463a5369518d5d35b288d21bd3a65a523b
surajkh <90398336+SurajKH@users.noreply.github.com>
2023-05-23 20:55:51 +0530

    Add files via upload

---
+++ frontend/inventoryfrontendapplication/src/Components/Orders.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NewProduct from './NewProducts';
+import NewOrders from './NewOrders';
+
+//we are defining the Orders Component over here.
+const Orders = () => {
+  return (
+    <React.Fragment>
+      <NewOrders/>
+    </React.Fragment>
+  )
+}
+
+export default Orders;
