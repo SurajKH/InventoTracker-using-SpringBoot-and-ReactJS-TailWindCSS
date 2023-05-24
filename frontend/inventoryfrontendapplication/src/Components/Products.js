30978a72d37935aab803bef5e52952154165d92c
surajkh <90398336+SurajKH@users.noreply.github.com>
2023-05-24 10:06:58 +0530

    Add files via upload

---
+++ frontend/inventoryfrontendapplication/src/Components/Products.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NewProduct from './NewProducts';
+
+//We are defining the Products Component over here.
+
+const Products = () => {
+  return (
+    <React.Fragment>
+      <NewProduct/>
+    </React.Fragment>
+  )
+}
+
+export default Products;
