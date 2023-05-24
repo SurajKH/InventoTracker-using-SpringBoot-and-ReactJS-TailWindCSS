a1a8ed881ad387f69d6a33d5b2bfa5d24358b49e
surajkh <90398336+SurajKH@users.noreply.github.com>
2023-05-24 10:05:23 +0530

    Update NewOrders.js

--- frontend/inventoryfrontendapplication/src/Components/NewOrders.js
+++ frontend/inventoryfrontendapplication/src/Components/NewOrders.js
@@ -159,13 +159,13 @@ className="h-10 w-96 border mt-2 px-2 py-2"
     className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mt-12 width:50% mx-auto justify-center"
     >Submit</button>
 
-    <button  onClick={(e)=>
+    {/* <button  onClick={(e)=>
             {
                console.log(neworder);
             }}
     className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mt-12 width:50% mx-auto justify-center ml-2">
     Clear
-    </button>
+    </button> */}
 
     {/* <input 
     type="text" 
